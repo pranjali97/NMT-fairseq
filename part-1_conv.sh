89affef5771b570938b63944d17b6aa12aa0dad0
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2023-04-21 09:05:58 -0700

    Update part-1_conv.sh

--- part-1_conv.sh
+++ part-1_conv.sh
@@ -11,11 +11,13 @@ fairseq-train \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --optimizer nag --clip-norm 0.1 \
     --lr 0.5 --lr-scheduler fixed --force-anneal 50 \
-    --max-tokens 3000 \
+    --max-tokens 3000 \ 
+    --max-epoch 2 \
     --save-dir checkpoints/fconv
 
 # # Evaluate
 fairseq-generate \
     fr-en/preprocessed \
     --path ccheckpoints/fconv/checkpoint_best.pt \
+    --scoring sacrebleu \
     --beam 5 --remove-bpe
